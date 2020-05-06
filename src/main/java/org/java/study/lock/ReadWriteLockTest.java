bf430052a4dadc7dba826f1f59fc73048e7924fc
fuqiang <qiang.fu@pingcl.com>
2020-05-06 11:11:00 +0800

    增加测试

--- src/main/java/org/java/study/lock/ReadWriteLockTest.java
+++ src/main/java/org/java/study/lock/ReadWriteLockTest.java
@@ -16,30 +16,23 @@ public class ReadWriteLockTest {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 100; i++) {
 			new Thread() {
 				public void run() {
-					try {
-						sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
 					reentrantReadWriteLock.readLock().lock();
-					System.out.println("read");
+					System.out.println("read:"+value);
 					reentrantReadWriteLock.readLock().unlock();
-					System.out.println(value);
 				}
 			}.start();
 		}
 
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 100; i++) {
 			new Thread() {
 				public void run() {
 					reentrantReadWriteLock.writeLock().lock();
-					System.out.println("write");
 					value = UUID.randomUUID().toString();
+					System.out.println("write:"+value);
 					reentrantReadWriteLock.writeLock().unlock();
-					System.out.println(value);
 				}
 			}.start();
 		}
---
+++ src/main/java/org/java/study/lock/StampedLockTest.java
@@ -0,0 +1,38 @@
+package org.java.study.lock;
+
+import java.util.UUID;
+import java.util.concurrent.locks.StampedLock;
+
+public class StampedLockTest {
+
+	public static StampedLock stampedLock = new StampedLock();
+
+	public static String uuid;
+
+	public static void main(String[] args) {
+
+		//write 
+		for (int i = 0; i < 1000; i++) {
+			new Thread() {
+				public void run() {
+					long stamp= stampedLock.writeLock();
+					uuid=UUID.randomUUID().toString();
+					System.out.println("write:"+uuid);
+					stampedLock.unlock(stamp);
+				}
+			}.start();
+		}
+		
+		//read
+		for(int i=0;i<1000;i++) {
+			new Thread() {
+				public void run() {
+					long stamp= stampedLock.readLock();
+					System.out.println("read:"+uuid);
+					stampedLock.unlock(stamp);
+				}
+			}.start();
+		}
+
+	}
+}
