c543ff4e7090e0574fd0173e9521684d394c2de7
fuqiang <qiang.fu@pingcl.com>
2020-05-07 18:43:55 +0800

    增加测试

---
+++ src/main/java/org/java/study/lock/CyclicBarrierTest.java
@@ -0,0 +1,24 @@
+package org.java.study.lock;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierTest {
+
+	private static CyclicBarrier cyclicBarrier = new CyclicBarrier(10);
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			final  int t=i;
+			new Thread() {
+				public void run() {
+					try {
+						sleep(t*1000);
+						cyclicBarrier.await();
+						System.out.println(System.currentTimeMillis());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}.start();
+		}
+	}
+}
