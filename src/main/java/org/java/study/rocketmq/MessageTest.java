d9965c24b5badbbc635339876f025cd0fc34db6b
fuqiang <qiang.fu@pingcl.com>
2020-05-12 15:57:52 +0800

    修改测试代码

--- src/main/java/org/java/study/rocketmq/MessageTest.java
+++ src/main/java/org/java/study/rocketmq/MessageTest.java
@@ -1,13 +1,11 @@
 package org.java.study.rocketmq;
 
+import java.io.File;
 import java.util.List;
 import java.util.UUID;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
-import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.MessageQueueSelector;
@@ -18,61 +16,64 @@ import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 
-import com.alibaba.fastjson.JSON;
-
 public class MessageTest {
 
 	public static String namesrvAddr = "127.0.0.1:9876";
 
-	public static String toptic = "ertiugdfk";
+	public static String toptic = "myTopic";
 
 	public static void main(String[] args) throws Exception {
-		DefaultMQProducer defaultMQProducer = new DefaultMQProducer("defaultMQProducer");
+		DefaultMQProducer defaultMQProducer = new DefaultMQProducer("myMQProducer");
 		defaultMQProducer.setNamesrvAddr(namesrvAddr);
+		//defaultMQProducer.setDefaultTopicQueueNums(1);
 		defaultMQProducer.start();
-		getDefaultMQPullConsumer("Consumer1");
-		getDefaultMQPullConsumer("Consumer2");
-		getDefaultMQPullConsumer("Consumer3");
-		getDefaultMQPullConsumer("Consumer4");
-		for (int i = 0; i < 1000; i++) {
-			Message message = new Message(toptic, String.valueOf(i).getBytes());
-			SendResult sendResult = defaultMQProducer.send(message, new MessageQueueSelector() {
+		for (int i = 0; i < 100; i++) {
+			Message message = new Message(toptic, String.valueOf(UUID.randomUUID().toString()+"_"+i).getBytes());
+			SendResult sendResult = defaultMQProducer.send(message,new MessageQueueSelector() {
 				@Override
 				public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
-					Integer id = (Integer) arg;
-                    int index = id % mqs.size();
-                    return mqs.get(index);
+//					int orderId = (int)arg;
+//					System.out.println(new String(msg.getBody()));	
+//					System.out.println(orderId % mqs.size());
+					return mqs.get( mqs.size()-1);
 				}
-			}, 0);
+			},i);
 			// System.out.println(JSON.toJSONString(sendResult));
 		}
-		// getDefaultMQPullConsumer();
-		
+		getDefaultMQPullConsumer("客户端1");
+		//getDefaultMQPullConsumer("客户端2");
+		//getDefaultMQPullConsumer("客户端2");
+
+		defaultMQProducer.shutdown();
 	}
 
 	public static DefaultMQPushConsumer getDefaultMQPullConsumer(String name) throws Exception {
-		DefaultMQPushConsumer defaultMQPullConsumer = new DefaultMQPushConsumer(UUID.randomUUID().toString());
+		DefaultMQPushConsumer defaultMQPullConsumer = new DefaultMQPushConsumer("myMQPushConsumer");
 		defaultMQPullConsumer.setNamesrvAddr(namesrvAddr);
 		defaultMQPullConsumer.setInstanceName(name);
 		defaultMQPullConsumer.subscribe(toptic, "*");
 		//defaultMQPullConsumer.
-		
+		defaultMQPullConsumer.registerMessageListener(new Listener() );
 		defaultMQPullConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
 		defaultMQPullConsumer.setMessageModel(MessageModel.CLUSTERING);
 //		defaultMQPullConsumer.setConsumeThreadMax(1);
 //		defaultMQPullConsumer.setConsumeThreadMin(1);
-//		defaultMQPullConsumer.setPullBatchSize(1);
-		defaultMQPullConsumer.registerMessageListener(new MessageListenerOrderly() {
-			@Override
-			public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
-				context.setAutoCommit(true);
-				for (MessageExt msg : msgs) {
-					System.out.println(defaultMQPullConsumer.getInstanceName() + "," + new String(msg.getBody()));
-				}
-				return ConsumeOrderlyStatus.SUCCESS;
-			}
-		});
+		defaultMQPullConsumer.setPullBatchSize(1);
+		
 		defaultMQPullConsumer.start();
 		return defaultMQPullConsumer;
+	}
+	
+	
+	public static class Listener implements MessageListenerOrderly{
+
+		@Override
+		public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
+			for (MessageExt msg : msgs) {
+				System.out.println(new String(msg.getBody()));	
+			}
+			return ConsumeOrderlyStatus.SUCCESS;
+		}
+		
 	}
 }
