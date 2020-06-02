cb3a117e14f4a5681e2eae694e6bdb4d3aefd4f3
fuqiang <fuqiang@DESKTOP-5FDRTHV>
2020-06-02 23:14:12 +0800

    增加测试代码

--- src/main/java/org/java/study/activeMQ/Consumer.java
+++ src/main/java/org/java/study/activeMQ/Consumer.java
@@ -28,13 +28,29 @@ public class Consumer {
 		MessageConsumer messageConsumer = session.createConsumer(q);
 		messageConsumer.setMessageListener(new MessageListener() {
 			public void onMessage(Message message) {
-				TextMessage textMessage=(TextMessage)message;
+				TextMessage textMessage = (TextMessage) message;
 				try {
+					System.out.println(textMessage.getJMSMessageID());
 					System.out.println(textMessage.getText());
 				} catch (JMSException e) {
 					e.printStackTrace();
 				}
 			}
 		});
+	}
+
+	public static MessageConsumer QueueConsumer() {
+		try {
+			ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(address);
+			Connection connection = activeMQConnectionFactory.createConnection();
+			connection.start();
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Queue q = session.createQueue(queue);
+			MessageConsumer messageConsumer = session.createConsumer(q);
+			return messageConsumer;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
--- src/main/java/org/java/study/activeMQ/Producer.java
+++ src/main/java/org/java/study/activeMQ/Producer.java
@@ -3,11 +3,13 @@ package org.java.study.activeMQ;
 import java.util.UUID;
 
 import javax.jms.Connection;
+import javax.jms.DeliveryMode;
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.jms.Topic;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQTextMessage;
@@ -16,25 +18,72 @@ public class Producer {
 
 	public static String queueName = "myQueue";
 
+	public static String topicName = "myTopic";
+
 	public static String address = "tcp://127.0.0.1:61616";
 
+	public static MessageProducer QueueMessageProduce = getQueueMessageProduce();
+
+	public static MessageProducer TopicMessageProduce = getQueueMessageProduce();
+
 	public static void main(String[] args) throws JMSException {
-		ActiveMQConnectionFactory activeMQConnectionFactory=new ActiveMQConnectionFactory(address);
-		Connection connection= activeMQConnectionFactory.createConnection();
-		connection.start();
-		Session session= connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		Queue queue=session.createQueue(queueName);
-		MessageProducer messageProducer=session.createProducer(queue);
-		//ActiveMQTextMessage ActiveMQTextMessage =new ActiveMQTextMessage();
-		for(int i=0;i<100;i++) {
-			TextMessage textMessage=session.createTextMessage("my text message "+i);
-			messageProducer.send(textMessage);
+		for (int i = 0; i < 100; i++) {
+			sendQueueText("sendQueueText_" + i);
+			sendTopicText("sendTopicText_" + i);
 		}
-//		while(true) {
-//			TextMessage textMessage=session.createTextMessage(UUID.randomUUID().toString());
-//			messageProducer.send(textMessage);
-//		}
-		session.close();
-		connection.close();
+	}
+
+	/**
+	 * 队列模式发文本消息
+	 * 
+	 * @param text
+	 */
+	public static void sendQueueText(String text) {
+		try {
+			TextMessage textMessage = new ActiveMQTextMessage();
+			textMessage.setText(text);
+			QueueMessageProduce.send(textMessage);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void sendTopicText(String text) {
+		try {
+			TextMessage textMessage = new ActiveMQTextMessage();
+			textMessage.setText(text);
+			TopicMessageProduce.send(textMessage);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static MessageProducer getQueueMessageProduce() {
+		try {
+			ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(address);
+			Connection connection = activeMQConnectionFactory.createConnection();
+			connection.start();
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Queue queue = session.createQueue(queueName);
+			MessageProducer messageProducer = session.createProducer(queue);
+			return messageProducer;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public static MessageProducer getTopicMessageProduce() {
+		try {
+			ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(address);
+			Connection connection = activeMQConnectionFactory.createConnection();
+			connection.start();
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Topic topic = session.createTopic(topicName);
+			MessageProducer messageProducer = session.createProducer(topic);
+			messageProducer.setDeliveryMode(DeliveryMode.PERSISTENT);
+			return messageProducer;
+		} catch (Exception e) {
+			return null;
+		}
 	}
 }
