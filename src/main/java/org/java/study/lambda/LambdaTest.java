c8d713a9ecb03b8541b8941f76dc4cdf5a29e4c4
fuqiang <fuqiang@DESKTOP-5FDRTHV>
2020-06-04 20:58:22 +0800

    增加lambda测试

---
+++ src/main/java/org/java/study/lambda/LambdaTest.java
@@ -0,0 +1,17 @@
+package org.java.study.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaTest {
+
+	public static void main(String[] args) {
+		List<String> list=new ArrayList<String>() {{
+			add("6533");
+			add("8799");
+		}};
+		list.stream().forEach(value->{
+			System.out.println(value);
+		});
+	}
+}
