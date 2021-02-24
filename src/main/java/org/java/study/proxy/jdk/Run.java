872b319e80c5d17768b44c3226a95a29bd0c08d1
fuqiang <653306065@qq.com>
2021-02-24 14:56:14 +0800

    修改代码

--- src/main/java/org/java/study/proxy/jdk/Run.java
+++ src/main/java/org/java/study/proxy/jdk/Run.java
@@ -2,8 +2,12 @@ package org.java.study.proxy.jdk;
 
 public class Run {
     public static void main(String[] args) {
-        DoThingImpl doThingImpl=new DoThingImpl();
-        DoThing doThing= (DoThing) JdkProxyFactory.getProxy(doThingImpl);
+        //旧版本jdk保存动态代理生成的class idea的工作空间下的 com\sun\proxy
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+        //新版本jdk保存动态代理生成的class idea的工作空间下的 com\sun\proxy
+        System.getProperties().put("jdk.proxy.ProxyGenerator.saveGeneratedFiles", "true");
+        DoThingImpl doThingImpl = new DoThingImpl();
+        DoThing doThing = (DoThing) JdkProxyFactory.getProxy(doThingImpl);
         doThing.work();
         doThing.play("英雄联盟");
     }
