b695e3828e06619ebd3f1343286008be014924e7
cookie <653306065@qq.com>
2021-04-26 23:21:06 +0800

    增加锁类型测试

--- src/main/java/org/java/study/thread/WaitNotifyTest.java
+++ src/main/java/org/java/study/thread/WaitNotifyTest.java
@@ -2,19 +2,52 @@ package org.java.study.thread;
 
 public class WaitNotifyTest {
 
-    public Integer value = 0;
+    public static Integer value = 0;
 
+    public Object lock = new Object();
+
+    //对象锁
+    public void addValueLock() {
+        synchronized (lock) {
+            value++;
+            lock.notifyAll();
+        }
+    }
+
+    //对象锁
+    public Integer getValueLock() throws InterruptedException {
+        synchronized (lock) {
+            lock.wait();
+            return value;
+        }
+    }
+
+    //实例锁
     public synchronized void addValue() {
         notifyAll();
         value++;
     }
 
+    //实例锁
     public synchronized Integer getValue() throws InterruptedException {
         wait();
         return value;
     }
 
+    //类锁
+    public synchronized static void addValueStatic() {
+        WaitNotifyTest.class.notifyAll();
+        value++;
+    }
+
+    //类锁
+    public synchronized static Integer getValueStatic() throws InterruptedException {
+        WaitNotifyTest.class.wait();
+        return value;
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        //实例锁测试
         WaitNotifyTest waitNotifyTest = new WaitNotifyTest();
         Thread thread1 = new Thread(() -> {
             try {
@@ -29,6 +62,37 @@ public class WaitNotifyTest {
         thread1.start();
         Thread.sleep(1);
         thread2.start();
+
+        //类锁测试
+        Thread thread3 = new Thread(() -> {
+            try {
+                System.out.println(getValueStatic());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread thread4 = new Thread(() -> {
+            addValueStatic();
+        });
+        thread3.start();
+        Thread.sleep(1);
+        thread4.start();
+
+        //对象锁测试
+        Thread thread5 = new Thread(() -> {
+            try {
+                System.out.println(waitNotifyTest.getValueLock());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread thread6 = new Thread(() -> {
+            waitNotifyTest.addValueLock();
+        });
+        thread5.start();
+        Thread.sleep(1);
+        thread6.start();
     }
+
 
 }
